99868ec004774f9d02ef0d87978c3b4110cf5a62
Dan Wexler <danwexler32@gmail.com>
2023-11-15 15:57:39 -0800

    markdown updates

--- rf/data_exploration_test.R
+++ rf/data_exploration_test.R
@@ -10,7 +10,7 @@ y <- y[y$ChangeType!="",]
 # removes patches without recovery information
 # y <- y[x$tcbPst07Mn!="0",]
 # x <- x[x$tcbPst07Mn!="0",]
-# removes extraneous columns (ids, years, etc.) and converts data to numeric
+# filters by variable type and converts data to numeric
 variables <- c("durMn","durSd","idxMagMn","idxMagSd","tcbMagMn","tcbMagSd",
                "tcgMagMn","tcgMagSd","tcwMagMn","tcwMagSd","tcbPreMn",
                "tcbPreSd","tcgPreMn","tcgPreSd","tcwPreMn","tcwPreSd",
@@ -29,11 +29,11 @@ disturbances <- c("Avalanche","Inter-annual Variability","Clearing",
                   "Fire","Post Fire Defoliation","Tree Toppling",
                   "Development","Post Clearing","Post Tree Toppling","Water")
 # EDIT 'disturbances' TO CHANGE DISTURBANCES INCLUDED IN MODEL
-disturbances <- disturbances[c(1,2,3,5,6,7,8,9)]
+disturbances <- disturbances[c(1,2,5,6,7,8,9)]
 # disturbances <- disturbances[c(1,2,3,4,5,6,7,8,9,11)]
 x <- x[y[,1]%in%disturbances,]
 y <- y[y[,1]%in%disturbances,,drop=FALSE]
-# y <- factor(y[y[,1]%in%disturbances,])
+# y <- factor(y[convert data frame to[,1]%in%disturbances,])
 # creates a training and testing set
 x_train <- data.frame(matrix(0,0,ncol(x)))
 y_train <- data.frame(matrix("",0,1))
@@ -43,19 +43,17 @@ for (disturbance in disturbances) {
   x_subset <- x[y[,1]==disturbance,]
   y_subset <- y[y[,1]==disturbance,,drop=FALSE]
   num_rows <- nrow(x_subset)
-  index_subset <- sample.int(num_rows,num_rows*0.8)
+  index_subset <- sample.int(num_rows,num_rows*0.75)
   x_train <- rbind(x_train,x_subset[index_subset,])
   y_train <- rbind(y_train,y_subset[index_subset,,drop=FALSE])
   x_test <- rbind(x_test,x_subset[-index_subset,])
   y_test <- rbind(y_test,y_subset[-index_subset,,drop=FALSE])
 }
-print(dim(x_train))
-print(dim(y_train))
-print(dim(x_test))
-print(dim(y_test))
+y_train <- factor(sapply(y_train,as.factor))
+y_test <- factor(sapply(y_test,as.factor))
 # -----------------------------------------------------------------------------
 # constructs vector used for sampling data and sets number of trees
-sample <- table(y)
+sample <- table(y_train)
 sample <- replace(sample,sample>100,100)
 sample_print <- as.data.frame(sample)
 colnames(sample_print) <- c("disturbance","freq")
@@ -63,7 +61,7 @@ colnames(sample_print) <- c("disturbance","freq")
 reps <- 50
 # runs random forest and extracts most important variables
 cat("initial rf run, finding best predictors","\r")
-forest <- randomForest(x=x,y=y,importance=TRUE,ntree=reps,sampsize=sample)
+forest <- randomForest(x=x_train,y=y_train,importance=TRUE,ntree=reps,sampsize=sample)
 gini <- forest$importance[,"MeanDecreaseGini"]
 # runs random forest using different combinations of variables
 # EDIT 'percentages' TO CHANGE PERCENTILE CUTOFFS FOR VARIABLES
@@ -77,10 +75,10 @@ colnames(predictions) <- sort(disturbances)
 min_class_error <- 99999
 best_variables <- NULL
 for (i in 1:length(percentages)) {
-  x_subset <- x[,gini>quantile(gini,probs=percentages[i])]
+  x_subset <- x_train[,gini>quantile(gini,probs=percentages[i])]
   cat(paste("rf iteration ",i,"/",length(percentages),
             " with the top ",ncol(x_subset)," predictors","\r",sep=""))
-  forest_subset <- randomForest(x=x_subset,y=y,importance=TRUE,ntree=reps,sampsize=sample)
+  forest_subset <- randomForest(x=x_subset,y=y_train,importance=TRUE,ntree=reps,sampsize=sample)
   gini_subset <- data.frame(forest_subset$importance[,"MeanDecreaseGini"])
   colnames(gini_subset) <- c("gini")
   results[i,1] <- ncol(x_subset)
@@ -106,12 +104,20 @@ message("***disturbance specific errors from all runs***")
 print(predictions)
 # -----------------------------------------------------------------------------
 # runs random forest with most predictive variables and prints results
-x_subset <- x[,best_variables]
-best_forest <- randomForest(x=x_subset,y=y,importance=TRUE,ntree=reps,sampsize=sample)
-message(paste("***confusion matrix for best run with ",ncol(x_subset)," variables***",sep=""))
-best_confusion <- best_forest$confusion
-best_confusion[,"class.error"] <- round(best_confusion[,"class.error"]*100,2)
-colnames(best_confusion)[length(colnames(best_confusion))] <- "error"
+x_train_subset <- x_train[,best_variables]
+x_test_subset <- x_test[,best_variables]
+best_forest <- randomForest(x=x_train_subset,y=y_train,xtest=x_test_subset,ytest=factor(y_test),importance=TRUE,ntree=reps,sampsize=sample)
+message(paste("***confusion matrix for best run with ",ncol(x_train_subset)," variables***",sep=""))
+confusion_train <- best_forest$confusion
+confusion_train[,"class.error"] <- round(confusion_train[,"class.error"]*100,2)
+colnames(confusion_train)[length(colnames(confusion_train))] <- "error"
 writeLines(paste("oob error: ",round(best_forest$err.rate[reps,"OOB"]*100,2),sep=""))
 writeLines(paste("avg class error: ",round(mean(best_forest$confusion[,"class.error"])*100,2),sep=""))
-print(best_confusion)
+print(confusion_train)
+confusion_test <- best_forest$test$confusion
+confusion_test[,"class.error"] <- round(confusion_test[,"class.error"]*100,2)
+colnames(confusion_test)[length(colnames(confusion_test))] <- "error"
+message(paste("***confusion matrix for test set with ",ncol(x_train_subset)," variables***",sep=""))
+writeLines(paste("avg class error: ",round(mean(best_forest$test$confusion[,"class.error"])*100,2),sep=""))
+print(confusion_test)
+# print(best_forest$test$votes)
